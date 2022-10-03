a8ca784d2f0cd4e74264f2c59c46668b8abd8c38
julioallday <naruto481@gmail.com>
2022-10-03 18:10:50 -0300

    Criação do projeto, funções e estrutura.

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var tempoInicial = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
+
+$(function(){
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+function atualizaTamanhoFrase () {
+  var frase = $(".frase").text();
+var numeroDePalavras = frase.split(" ").length;
+var tamanhoDaFrase = $("#tamanho-frase");
+tamanhoDaFrase.text(numeroDePalavras);
+}
+
+function inicializaContadores () {
+  campo.on("input", function(){
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres)
+  });
+}
+
+function inicializaCronometro () {
+  var tempoRestante = $("#tempo-digitacao").text();
+  $(campo).one("focus", function () { 
+    var cronometroID = setInterval(function(){
+      tempoRestante--;
+      $("#tempo-digitacao").text(tempoRestante);
+      if(tempoRestante < 1){
+          campo.attr("disabled", true);
+          clearInterval(cronometroID);
+        }
+      },1000);
+    }
+  );
+}
+
+function reiniciaJogo () {
+  $("#botao-reiniciar").click(function () {
+    campo.attr("disabled", false)
+    $(campo).val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+  });
+}
+
+  
