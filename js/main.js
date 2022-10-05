386bbba16106cd365134a7a3fffbd1b26e6c93dd
julioallday <naruto481@gmail.com>
2022-10-05 00:14:56 -0300

    Ajustes nas funções javascript

--- js/main.js
+++ js/main.js
@@ -46,37 +46,31 @@ function inicializaCronometro () {
   );
 }
 
-function inicializaMarcadores (){
-let frase = $(".frase").text();
-campo.on("input",function(){
-  let digitado = campo.val();
-  let comparavel = frase.substr(0, digitado.length)
-  if(digitado != comparavel) {
-    campo.addClass("borda-vermelha");
-    campo.removeClass("borda-verde");
+function inicializaMarcadores() {
+  var frase = $(".frase").text();
+  campo.on("input", function() {
+      var digitado = campo.val();
+      var comparavel = frase.substr(0 , digitado.length);
 
-  } else {
-    campo.addClass("borda-verde");
-    campo.removeClass("borda-vermelha");
-  }
-});
-}
-
-function reiniciaJogo () {
-  $("#botao-reiniciar").click(function () {
-    campo.attr("disabled", false)
-    $(campo).val("");
-    $("#contador-palavras").text("0");
-    $("#contador-caracteres").text("0");
-    $("#tempo-digitacao").text(tempoInicial);
-    inicializaCronometro();
-    campo.toggleClass("campo-desativado");
-
-    campo.removeClass("borda-vermelha");
-    campo.removeClass("borda-verde");
+      if(digitado == comparavel) {
+          campo.addClass("borda-verde");
+          campo.removeClass("borda-vermelha");
+      } else {
+          campo.addClass("borda-vermelha");
+          campo.removeClass("borda-verde");
+      }
   });
 }
 
+function reiniciaJogo(){
+  campo.attr("disabled",false);
+  campo.val("");
+  $("#contador-palavras").text("0");
+  $("#contador-caracteres").text("0");
+  $("#tempo-digitacao").text(tempoInicial);
+  inicializaCronometro();
+  campo.toggleClass("campo-desativado");
 
-
-  
+  campo.removeClass("borda-vermelha"); //novo
+  campo.removeClass("borda-verde"); //novo
+}
